169ef30738b138b97a4cdc4710b81d570aa9f049
PJ0707 <83937855+PJ0707@users.noreply.github.com>
2021-05-15 17:34:44 +0530

    Add files via upload

---
+++ tracking.php
@@ -0,0 +1,73 @@
+<?php
+session_start(); 
+$host="localhost"; 
+$user="FTS"; 
+$password=""; 
+$db="user_login_details"; 
+$port="3307";
+$con=mysqli_connect($host,$user,$password,$db,$port); 
+$id=$_SESSION['id']; 
+$query="select * from file_table where id='".$id."'"; 
+$result= mysqli_query($con,$query); 
+set_time_limit(500); 
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+ <meta charset="utf-8">
+ <meta content="width=device-width, initialscale=1.0" name="viewport">
+ <title>Track_page</title>
+ <meta content="" name="description">
+ <meta content="" name="keywords">
+ <style>
+ 	body
+ 	{
+ 		padding-bottom: 400px;
+ 		background:url(k.jpg);
+ 		background-position: center;
+ 		background-repeat: no-repeat;
+ 		background-size: cover;
+ 	}
+ </style>
+</head>
+<body>
+<img  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFVqXqqaIl4phDSDgqUq0M3nMzqnvxS17-Uw&usqp=CAU" width="250" height="200">
+<a href="selection.php"><button style="float: right; "class="w3-button w3-red w3-round-large">Go back >></button></a>
+<a href="logout.php"><button style = "float:right;" class="button"><span id="font1">LogOut </span></button></a>
+<form method="POST" action="<?php echo htmlentities($_SERVER['PHP_SELF']);?>" >
+<?php
+ if (mysqli_num_rows($result) > 0) { 
+ while($row = mysqli_fetch_assoc($result)) { 
+ $fid = $row["fileid"] ; 
+ $name = $row["name"] ; 
+ 
+ ?> 
+ <span><?php echo $fid?></span>
+ <span><?php echo $name?> </span> 
+<div>
+  <input style="float:right;" type="submit" name="<?php echo $fid; ?>" value="track"> 
+</div>
+</form>
+<?php
+extract($_REQUEST); 
+$query="select fileid from file_table where id='".$id."'"; 
+$result= mysqli_query($con,$query); 
+while($row = mysqli_fetch_assoc($result)) { 
+ $rid=$row["fileid"]; 
+ if(isset($_POST[$rid])) 
+ { 
+ $_SESSION["rid"] = $rid; 
+ ?>
+ <script>
+ 
+ location.replace("status.php"); 
+ </script>
+ <?php
+ 
+ } 
+} 
+?>
+
+</body>
+</html>
